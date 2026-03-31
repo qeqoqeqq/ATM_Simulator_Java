767c6558c7d23001ad5d4737c7033644cc132f44
qeqoqeqq <goshanezhuta228@gmail.com>
2026-03-31 14:01:43 +0200

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package atm;
 
-/**
- *
- * @author famil
- */
+
 public class Card {
     private final String cardnum;
     private final String PIN;
